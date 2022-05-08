4dcb7cfd797d5fcab39a7150caafc4e16a6b3df1
Nicholas Park <npark@pplweb.com>
2022-05-09 06:05:54 +0900

    Add sources

---
+++ bluepill/build/generated/source/buildConfig/release/com/cincinnatiai/bluepill/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.cincinnatiai.bluepill;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.cincinnatiai.bluepill";
+  public static final String BUILD_TYPE = "release";
+}
---
+++ bluepill/build/intermediates/java_doc_dir/release/module-search-index.js
@@ -0,0 +1,1 @@
+var moduleSearchIndex = [{"l":"bluepill","url":"index.html"}]
